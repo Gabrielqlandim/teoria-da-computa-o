bebc10ab28c5da4960b9efd06709a42052d9f42f
PedrooSam <pssa@cesar.school>
2025-05-10 22:12:26 -0300

    Código em C finalizado

--- mergeSort.c
+++ mergeSort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void intercala(int inicio, int meio, int fim, int v[], int tam){
     int inicio_v1 = inicio;
@@ -32,19 +34,86 @@ void mergeSort(int inicio, int fim, int v[], int tam){
     }
 }
 
-int main(void){
-    int v[] = {38, 27, 43, 3, 9, 82, 10};
 
-    int tam = sizeof(v)/sizeof(v[0]);
+void executar(int tam, int caso){
 
-    mergeSort(0, tam-1, v, tam);
+    double tempos_execucao[1000];
 
-    printf("Vetor em ordem: ");
+    //Laço de reetição para realizar a ordenação 1000 vezes para calcular a média
+    for(int j=0; j<1000; j++){
 
-    for(int i=0; i<tam; i++)
-        printf("%d ", v[i]);
-    
+        int v[tam];
+        clock_t inicio, fim;
+        double tempo;
+
+        //Laço de repetição para inicializar o vetor
+        for(int i=0; i<tam; i++){
+
+            //MELHOR CASO - inicia o vetor ordenado
+            if (caso == 1){
+                v[i] = i;
+            }
+            //CASO MÉDIO - inicia o vetor com números aleatórios
+            else if (caso == 2){
+                v[i] = rand();
+            }
+            //PIOR CASO - cria o vetor ordenado descrescente
+            else if (caso == 3){
+                v[i] = tam - i;
+            }
+        }
+
+        //Roda o merge sort e salva os tempos de inicio e fim da execução
+        inicio = clock();
+        mergeSort(0, tam-1, v, tam);
+        fim = clock();
+
+        //Calcula o tempo de execução do código e adiciona na lista de tempos
+        tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+        tempos_execucao[j] = tempo;
+        
+    }
+
+    //Calcula a média de tempo de execução
+    double soma = 0.0;
+    double media;
+
+    for (int i = 0; i < 1000; i++){
+        soma += tempos_execucao[i];
+    }
+
+    media = soma/1000;
+
+    if (caso == 1){
+        printf("Melhor caso\n");
+    }
+    else if (caso == 2){
+        printf("Caso medio\n");
+    }
+    else if (caso == 3){
+        printf("Pior caso\n");
+    }
+
+    printf("Vetor de tamanho %d\n", tam);
+    printf("Media de tempo: %f segundos\n", media);
     printf("\n");
 
+}
+
+int main(void){
+
+    executar(100, 1);
+    executar(100, 2);
+    executar(100, 3);
+
+    executar(1000, 1);
+    executar(1000, 2);
+    executar(1000, 3);
+
+    executar(10000, 1);
+    executar(10000, 2);
+    executar(10000, 3);
+
     return 0;
+    
 }
